d1d63009e499586222cfeffec6ab56ac0dde77ad
GucioWons <wgutek1@wp.pl>
2022-01-10 19:31:13 +0100

    xd

--- Accounts/models.py
+++ Accounts/models.py
@@ -3,6 +3,9 @@ from django.db import models
 
 # Create your models here.
 from django.urls import reverse
+from django.utils import timezone
+
+from Posts.models import Post
 
 
 class Account(models.Model):
@@ -18,3 +21,16 @@ class Account(models.Model):
         return reverse('accounts:add-to-friends', kwargs={'my_id': self.id})
     def get_remove_from_friends_url(self):
         return reverse('accounts:remove-from-friends', kwargs={'my_id': self.id})
+
+class Notification(models.Model):
+    date = models.DateTimeField(default=timezone.now)
+    user = models.OneToOneField(User, unique=True, on_delete=models.CASCADE)
+    new = models.BooleanField(default=True)
+
+
+class Request(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Action(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    type= models.CharField(max_length=50)
