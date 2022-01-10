7c58ed91b6d5d913ff86a36153db918b5ca46a1a
baczi <256470@student.pwr.edu.pl>
2022-01-10 19:14:52 +0100

    podstawy powiadomienia

--- Accounts/admin.py
+++ Accounts/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from Accounts.models import Account
+from Accounts.models import Account, Notification, Request, Action
 
 admin.site.register(Account)
+admin.site.register(Notification)
+admin.site.register(Request)
+admin.site.register(Action)
+
